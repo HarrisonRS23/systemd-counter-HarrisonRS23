b9f521a8d90e9cc67f0b9e17b60f7d579658804a
HarrisonRS23 <82061362+HarrisonRS23@users.noreply.github.com>
2024-02-07 02:45:56 +0000

    updated code to accept signals

--- secondsScript.py
+++ secondsScript.py
@@ -1,17 +1,49 @@
 import time
+import signal
+import sys
 from datetime import date, datetime
 
-out_file = open('test.txt', 'w')
+# Our signal handler
+# def signal_handler(signum, frame):  
+ #   print("Signal Number:", signum, " Frame: ", frame)  
+ 
+# def exit_handler(signum, frame):
+ #    print('Exiting....')
+ #    exit(0)
+
+def term_handler(signum, frame):
+    out = "Harrison: " + str(date.today()) + " " + str(datetime.now().strftime("%H:%M:%S")) + " Recieved SIGTERM, exiting..."
+    print (out, flush=True)
+    out_file.write(out + '\n')
+    out_file.flush()
+    out_file.close()
+    sys.exit()
+ 
+# Register our signal handler with `SIGINT`(CTRL + C)
+# signal.signal(signal.SIGINT, signal_handler)
+
+# Register our signal handler with `SIGTERM`
+signal.signal(signal.SIGTERM, term_handler)
+ 
+# Register the exit handler with `SIGTSTP` (Ctrl + Z)
+# signal.signal(signal.SIGTSTP, exit_handler)
+try:
+    out_file = open('/tmp/currentCount.out', 'w')
+except PermissionExeption as e:
+    print("Error: ", e)
+    sys.exit()
+
 counter = 0
 while True:
     out = "Harrison: " + str(date.today()) + " " + str(datetime.now().strftime("%H:%M:%S")) + " #" + str(counter)
-    print(out, flush=True)
+    print(out)
     out_file.write(out + '\n')
-    out_file.flush()
+    # out_file.flush()
     # flush = true print instantly
     counter += 1
     time.sleep(1)
 
+out_file.close()
 # delete temp file after finishing script will crash otherwise
 # ls -la and keep output then print out to see
 # check permissions
