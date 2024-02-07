dd6a95a7331933809b3771970caa537195d3754c
Harriso <johndoe@example.com>
2024-02-07 18:40:30 +0000

    working service code

--- secondsScript.py
+++ secondsScript.py
@@ -1,15 +1,8 @@
 import time
 import signal
 import sys
+import os
 from datetime import date, datetime
-
-# Our signal handler
-# def signal_handler(signum, frame):  
- #   print("Signal Number:", signum, " Frame: ", frame)  
- 
-# def exit_handler(signum, frame):
- #    print('Exiting....')
- #    exit(0)
 
 def term_handler(signum, frame):
     out = "Harrison: " + str(date.today()) + " " + str(datetime.now().strftime("%H:%M:%S")) + " Recieved SIGTERM, exiting..."
@@ -18,18 +11,13 @@ def term_handler(signum, frame):
     out_file.flush()
     out_file.close()
     sys.exit()
- 
-# Register our signal handler with `SIGINT`(CTRL + C)
-# signal.signal(signal.SIGINT, signal_handler)
 
 # Register our signal handler with `SIGTERM`
 signal.signal(signal.SIGTERM, term_handler)
- 
-# Register the exit handler with `SIGTSTP` (Ctrl + Z)
-# signal.signal(signal.SIGTSTP, exit_handler)
+
 try:
-    out_file = open('/tmp/currentCount.out', 'w')
-except PermissionExeption as e:
+    out_file = open('/tmp/currentCount.out' , 'w')
+except PermissionError as e:
     print("Error: ", e)
     sys.exit()
 
@@ -42,14 +30,4 @@ while True:
     # flush = true print instantly
     counter += 1
     time.sleep(1)
-
-out_file.close()
-# delete temp file after finishing script will crash otherwise
-# ls -la and keep output then print out to see
-# check permissions
-# make a file in the way and try and run it
-# journal ctl and log files to debug
-# try: open file
-# except PermissionExeption as E
-# print (e)
-# die
+    
